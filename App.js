fb56b5bb8ce236d212cee6047ad281c3139f10c1
IgnazioFausto <nachojam777@gmail.com>
2022-04-20 22:24:46 -0300

    primer commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Vamos con React Native!</Text>
+      
     </View>
   );
 }
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+  }
 });
